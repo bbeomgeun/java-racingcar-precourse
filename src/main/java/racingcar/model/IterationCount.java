85df85a87f4d745d2c421a90d52289abe0744e34
bbeomgeun <miki308@naver.com>
2021-12-06 20:44:36 +0900

    feat(count): 시도 횟수에 대한 검증 로직이 포함된 VO 추가

---
+++ src/main/java/racingcar/model/IterationCount.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+public class IterationCount {
+    private final int iterationCount;
+
+    public IterationCount(String iterationCount) throws IllegalArgumentException {
+        if (!isNumber(iterationCount)) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 한다.");
+        }
+
+        if (!isValidRange(iterationCount)) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 1 이상이어야 합니다.");
+        }
+        this.iterationCount = Integer.parseInt(iterationCount);
+    }
+
+    private boolean isNumber(String count) {
+        return count.chars().allMatch(Character::isDigit);
+    }
+
+    private boolean isValidRange(String count) {
+        return Integer.parseInt(count) >= 1;
+    }
+
+    public int getIterationCount() {
+        return this.iterationCount;
+    }
+}
